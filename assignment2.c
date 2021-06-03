cab85b6c023909e7eb1ff290c963d85782b8b514
Not-A-Bot-1 <85301388+Not-A-Bot-1@users.noreply.github.com>
2021-06-03 20:32:09 +0530

    Add files via upload

---
+++ assignment2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("            ASSIGNEMENT - 2              ");
+printf("\nEnter the character in small letters:");
+scanf("%c",&ch);
+if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+{
+printf("\n The letter is a vowel:)");
+}
+else{
+printf("\n The letter is not a vowel:(");
+}
+}
